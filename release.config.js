3ed62c4ebaf13cb3572b68dfc7efce3429cabd17
uikinion <alessandra.zhosan@gmail.com>
2025-04-01 17:39:55 +0700

    Add files via upload

---
+++ release.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  branches: ["main"], // Указываем, для какой ветки делать релиз
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
+
