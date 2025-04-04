55f76b440faf773dce7b6240a94fa903e95f86b5
uikinion <alessandra.zhosan@gmail.com>
2025-04-04 16:38:26 +0700

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["main"], // Указываем, для какой ветки делать релиз
+  branches: ["main"], 
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
